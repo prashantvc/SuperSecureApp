f349be9ae60dd82bb9b1e96de48aa128697a327c
Prashant Cholachagudda <pvc@outlook.com>
2017-01-05 17:46:20 +0530

    Added the TouchID support

--- SuperSecureApp/ViewController.cs
+++ SuperSecureApp/ViewController.cs
@@ -2,6 +2,7 @@
 
 using AppKit;
 using Foundation;
+using LocalAuthentication;
 
 namespace SuperSecureApp
 {
@@ -15,8 +16,70 @@ namespace SuperSecureApp
 		{
 			base.ViewDidLoad();
 
-			// Do any additional setup after loading the view.
+			UseTouchID();
 		}
+
+		void UseTouchID()
+		{
+			var contex = new LAContext();
+			NSError authError;
+			if (contex.CanEvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, out authError))
+			{
+				replyHandler = new LAContextReplyHandler((success, error) =>
+				{
+					if (success)
+					{
+						InvokeOnMainThread(()=>ShowMessage());
+					}
+				});
+			}
+
+			contex.EvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, "authenticate", replyHandler);
+		}
+
+
+		partial void SignUpClicked(NSObject sender)
+		{
+			string username = Username.StringValue;
+			if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(Password.StringValue))
+				return;
+
+			KeychainHelpers.SetPasswordForUsername(username, Password.StringValue, SecureId,
+												   Security.SecAccessible.Always, true);
+
+			NSUserDefaults.StandardUserDefaults.SetString(username, "username");
+			NSUserDefaults.StandardUserDefaults.Synchronize();
+
+		}
+
+		partial void LoginClicked(NSObject sender)
+		{
+			string username = NSUserDefaults.StandardUserDefaults.StringForKey("username");
+			string password = KeychainHelpers.GetPasswordForUsername(Username.StringValue, SecureId, true);
+
+			if (Username.StringValue == "guest")
+			{
+				ShowMessage();
+			}
+		}
+
+		partial void TouchIDClicked(NSObject sender)
+		{
+			UseTouchID();
+		}
+
+		void ShowMessage(string message = "Here's your boring message")
+		{
+			var alert = new NSAlert();
+			alert.MessageText = "Secrete Message";
+			alert.InformativeText = message;
+
+			alert.AlertStyle = NSAlertStyle.Informational;
+
+			alert.BeginSheet(this.View.Window);
+
+		}
+
 
 		public override NSObject RepresentedObject
 		{
@@ -30,5 +93,8 @@ namespace SuperSecureApp
 				// Update the view, if already loaded.
 			}
 		}
+
+		const string SecureId = "SuperSecureApp";
+		LAContextReplyHandler replyHandler;
 	}
 }
--- SuperSecureApp/ViewController.designer.cs
+++ SuperSecureApp/ViewController.designer.cs
@@ -1,18 +1,67 @@
-﻿// WARNING
+// WARNING
 //
 // This file has been generated automatically by Xamarin Studio to store outlets and
 // actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace SuperSecureApp
 {
-	[Register("ViewController")]
+	[Register ("ViewController")]
 	partial class ViewController
 	{
-		void ReleaseDesignerOutlets()
+		[Outlet]
+		AppKit.NSButton Login { get; set; }
+
+		[Outlet]
+		AppKit.NSSecureTextField Password { get; set; }
+
+		[Outlet]
+		AppKit.NSButton SignUpButton { get; set; }
+
+		[Outlet]
+		AppKit.NSButton TouchIDButton { get; set; }
+
+		[Outlet]
+		AppKit.NSTextField Username { get; set; }
+
+		[Action ("LoginClicked:")]
+		partial void LoginClicked (Foundation.NSObject sender);
+
+		[Action ("SignUpClicked:")]
+		partial void SignUpClicked (Foundation.NSObject sender);
+
+		[Action ("TouchIDClicked:")]
+		partial void TouchIDClicked (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
 		{
+			if (Password != null) {
+				Password.Dispose ();
+				Password = null;
+			}
+
+			if (Username != null) {
+				Username.Dispose ();
+				Username = null;
+			}
+
+			if (TouchIDButton != null) {
+				TouchIDButton.Dispose ();
+				TouchIDButton = null;
+			}
+
+			if (SignUpButton != null) {
+				SignUpButton.Dispose ();
+				SignUpButton = null;
+			}
+
+			if (Login != null) {
+				Login.Dispose ();
+				Login = null;
+			}
 		}
 	}
 }
